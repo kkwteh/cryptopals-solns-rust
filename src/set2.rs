47a001ce645b1fe97a80b23304796a455ad5ad97
Kevin Teh <kevin@vinequai.com>
2021-01-08 02:16:17 +0100

    Test SimpleCbc encrypt-decrypt

--- src/set2.rs
+++ src/set2.rs
@@ -34,6 +34,31 @@ mod set2 {
         simple_cbc.update(&input, output.as_mut_slice()).unwrap();
     }
 
+    #[test]
+    fn test_simple_cbc() {
+        println!("Encrypting");
+        let key = "YELLOW SUBMARINE".to_owned().into_bytes();
+        let input = "CHARTREUSE DONUTCHARTREUSE DONUTCHARTREUSE DONUT"
+            .to_owned()
+            .into_bytes();
+        println!("Input {:?}", &input);
+        println!("Input length {:?}", &input.len());
+        let iv: Vec<u8> = vec![0u8; BLOCK_SIZE];
+        let mut simple_cbc = SimpleCbc::new(&key, symm::Mode::Encrypt, iv);
+        let mut output: Vec<u8> = vec![0u8; input.len() + BLOCK_SIZE];
+        simple_cbc.update(&input, output.as_mut_slice()).unwrap();
+        println!("Output {:?}", &output[0..48]);
+        println!("Decrypting");
+        let iv: Vec<u8> = vec![0u8; BLOCK_SIZE];
+        let mut simple_cbc = SimpleCbc::new(&key, symm::Mode::Decrypt, iv);
+        let mut decrypt_output: Vec<u8> = vec![0u8; input.len() + BLOCK_SIZE];
+        simple_cbc
+            .update(&output[0..48], decrypt_output.as_mut_slice())
+            .unwrap();
+        println!("Decrypted output {:?}", &decrypt_output[0..48]);
+        assert_eq!(&input[..], &decrypt_output[0..input.len()]);
+    }
+
     pub struct SimpleCbc {
         ecb: SimpleEcb,
         iv: Vec<u8>,
@@ -53,36 +78,56 @@ mod set2 {
             // CBC mode visual https://en.wikipedia.org/wiki/Block_cipher_mode_of_operation#Cipher_block_chaining_(CBC)
             // Assume input is a multiple of 16
             let num_blocks = input.len() / BLOCK_SIZE;
-            println!("Num blocks: {:?}", num_blocks);
             match self.mode {
                 symm::Mode::Encrypt => {
-                    let prev_cipher_text = &self.iv;
                     for i in 0..num_blocks {
                         let input_block = &input[i * BLOCK_SIZE..(i + 1) * BLOCK_SIZE];
-                        println!("Block {:?}", i);
-                        println!("Input block {:?}", &input_block);
-                        let preprocessed_block =
-                            xor_bytes(input_block.iter(), prev_cipher_text.iter());
-                        println!("Preprocessed block length {:?}", &preprocessed_block.len());
+                        let chain_block: std::slice::Iter<u8>;
+                        if i == 0 {
+                            chain_block = self.iv.iter();
+                        } else {
+                            chain_block = output[(i - 1) * BLOCK_SIZE..i * BLOCK_SIZE].iter();
+                        }
+                        let preprocessed_block = xor_bytes(input_block.iter(), chain_block);
                         self.ecb.update(
                             &preprocessed_block,
                             &mut output[i * BLOCK_SIZE..(i + 2) * BLOCK_SIZE],
                         )?;
-                        let prev_cipher_text = output[i * BLOCK_SIZE..(i + 1) * BLOCK_SIZE].iter();
+                        println!(
+                            "After update output block {:?}",
+                            &output[i * BLOCK_SIZE..(i + 2) * BLOCK_SIZE]
+                        );
                     }
                 }
                 symm::Mode::Decrypt => {
-                    let xor_text = &self.iv;
+                    let mut pre_xor_output: Vec<u8> = vec![0u8; input.len() + BLOCK_SIZE];
+                    self.ecb.update(&input, &mut pre_xor_output)?;
                     for i in 0..num_blocks {
-                        let input_block = &input[i * BLOCK_SIZE..(i + 1) * BLOCK_SIZE];
-                        println!("Block {:?}", i);
-                        let mut pretext: Vec<u8> = vec![0u8; 2 * BLOCK_SIZE];
-                        self.ecb.update(&input_block, &mut pretext)?;
-                        let next_output_block =
-                            xor_bytes(pretext[0..BLOCK_SIZE].iter(), xor_text.iter());
+                        let chain_block: std::slice::Iter<u8>;
+                        if i == 0 {
+                            chain_block = self.iv.iter();
+                        } else {
+                            chain_block = input[(i - 1) * BLOCK_SIZE..i * BLOCK_SIZE].iter();
+                        }
+                        println!(
+                            "input block {:?}",
+                            &input[i * BLOCK_SIZE..(i + 1) * BLOCK_SIZE]
+                        );
+                        println!(
+                            "pre_xor_output {:?}",
+                            &pre_xor_output[i * BLOCK_SIZE..(i + 1) * BLOCK_SIZE]
+                        );
+                        println!("chain_block {:?}", &chain_block);
+                        let next_output_block = xor_bytes(
+                            pre_xor_output[i * BLOCK_SIZE..(i + 1) * BLOCK_SIZE].iter(),
+                            chain_block,
+                        );
                         &output[i * BLOCK_SIZE..(i + 1) * BLOCK_SIZE]
-                            .copy_from_slice(&next_output_block);
-                        let xor_text = &output[i * BLOCK_SIZE..(i + 1) * BLOCK_SIZE];
+                            .copy_from_slice(&next_output_block[0..BLOCK_SIZE]);
+                        println!(
+                            "After update output block {:?}",
+                            &output[i * BLOCK_SIZE..(i + 1) * BLOCK_SIZE]
+                        );
                     }
                 }
             }
