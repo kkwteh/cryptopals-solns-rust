e10e7a3b0544d02d3910ebf3b1be2033aa045854
Kevin Teh <kevin@vinequai.com>
2021-01-23 00:39:40 +0100

    Challenge 26 success

--- src/set4.rs
+++ src/set4.rs
@@ -8,6 +8,7 @@ mod set3 {
     use lazy_static::lazy_static;
     use openssl::symm;
     use rand;
+    use rand::distributions::Alphanumeric;
     use rand::Rng;
     use std::convert::TryInto;
     use std::fs;
@@ -17,6 +18,9 @@ mod set3 {
     use std::time::SystemTime;
     lazy_static! {
         static ref KEY: Vec<u8> = random_block();
+    }
+    lazy_static! {
+        static ref NONCE: Vec<u8> = (0..8).map(|_| rand::thread_rng().gen::<u8>()).collect();
     }
 
     #[test]
@@ -34,8 +38,7 @@ mod set3 {
         // With the edit function, it's easy to break the CTR. Just edit with a known text,
         // and then XOR the edited text with the known text to recover the key stream.
         // Then XOR the keystream with the ciphertext to recover the original plaintext.
-        let nonce: Vec<u8> = vec![0u8; 8];
-        let mut simple_ctr = SimpleCtr::new(&KEY, nonce);
+        let mut simple_ctr = SimpleCtr::new(&KEY, NONCE.clone());
         let mut ciphertext: Vec<u8> = vec![0u8; input.len()];
         // We trim out the extra block added to plaintext for padding purposes
         simple_ctr
@@ -52,9 +55,54 @@ mod set3 {
         println!("{:?}", recovered_plaintext);
     }
 
-    fn test_edit(ciphertext: &[u8], key: &[u8], offset: usize, newtext: &[u8]) -> Vec<u8> {
-        let nonce: Vec<u8> = vec![0u8; BLOCK_SIZE];
-        let simple_ctr = SimpleCtr::new(&KEY, nonce);
-        vec![0u8]
+    fn challenge_26_oracle(input: &str) -> Vec<u8> {
+        let prefix = "comment1=cooking%20MCs;userdata=";
+        let postfix = ";comment2=%20like%20a%20pound%20of%20bacon";
+        let input = input
+            .replace("\\", "\\\\")
+            .replace(";", "\\;")
+            .replace("=", "\\=");
+        let plaintext = [prefix, &input, postfix].concat().to_owned();
+        let plaintext_bytes: Vec<u8> = plaintext.as_bytes().to_vec();
+        let mut ctr = SimpleCtr::new(&KEY, NONCE.clone());
+        let mut output: Vec<u8> = vec![0u8; plaintext_bytes.len()];
+        ctr.update(&plaintext_bytes, &mut output);
+        output
+    }
+
+    fn challenge_26_decrypt(input: &[u8]) -> Result<String, std::str::Utf8Error> {
+        let mut ctr = SimpleCtr::new(&KEY, NONCE.clone());
+        let mut output: Vec<u8> = vec![0u8; input.len()];
+        ctr.update(input, &mut output).unwrap();
+        Ok(str::from_utf8(&output)?.to_owned())
+    }
+
+    fn challenge_26_is_admin(input: &str) -> bool {
+        match input.find(";admin=true;") {
+            Some(_) => true,
+            None => false,
+        }
+    }
+
+    #[test]
+    fn challenge_26() {
+        // For CTR mode, the bit flip attack is even easier. Since encryption / decryption happens
+        // by XOR'ing with the keystream, we just need to flip any bit of the ciphertext
+        // to flip the corresponding bit of the plaintext
+
+        let input = ":admin<true";
+        let mut encrypted = challenge_26_oracle(&input);
+        // Prefix is 32 bytes long. We want to flip the last bit for index 32 and index 38
+        encrypted[32] = encrypted[32] ^ 1u8;
+        encrypted[38] = encrypted[38] ^ 1u8;
+        let decrypted = challenge_26_decrypt(&encrypted);
+        match decrypted {
+            Ok(value) => {
+                println!("Success with input {:?}", input);
+                println!("Decryption of modified ciphertext: {:?}", value);
+                return;
+            }
+            Err(_) => {}
+        }
     }
 }
